ec2a13b33fa794d22f55175c03c56e3bc56dd128
abbot <abbot@YURILAPTOP.local>
2023-09-25 15:59:15 -0600

    So, here is the helloWorld source from my Hello, WORLD! tutorial. I hope this helps you some how. :)

---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is just another copy of the helloWorld source from my second tutorial
+ * Hello, WORLD!
+ */
+
+int main(int argc, char* argv[]) {
+	printf("Hello, World!\n");
+
+	return 0;
+}
